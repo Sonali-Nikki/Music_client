cdc2cf820be4de2c37c463f9e6a1d483e4138c7b
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2026-01-03 12:48:59 +0530

    initial commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://music-server-id7q.onrender.com/api",
+  baseURL: "https://music-server-id7q.onrender.com",
   withCredentials: true,
 });
 
